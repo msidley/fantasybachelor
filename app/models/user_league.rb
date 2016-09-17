0a80634b51d6333751fb200dbe069438a0fe3eb2
Matthew Sidley <msidley@chicagobooth.edu>
2016-09-17 10:44:30 +0200

    user_league assoc&validations

--- app/models/user_league.rb
+++ app/models/user_league.rb
@@ -2,6 +2,19 @@ class UserLeague < ActiveRecord::Base
 
 has_many :comments, :dependent => :destroy
 
+has_many :fantasy_contestants, :dependent => :destroy
+
 has_many :picked_contestants, :through => :fantasy_contestants, :source => :cast
+
+belongs_to :user
+
+belongs_to :league
+
+validates :user_id, :presence => true
+validates :league_id, :presence => true
+validates :point_total, :presence => true
+validates :points_last_week, :presence => true
+validates :league_locked, :presence => true
+validates :commissioner, :presence => true
 
 end
